3b5a6367b734a4b63318177540e521591a6285d7
clloveras76 <celloveras@hotmail.com>
2015-05-09 00:23:24 -0300

    Nuestra_Historia_19
    Nuestra_Historia_19 + bton top en todas las paginas

--- js/scripts.js
+++ js/scripts.js
@@ -52,7 +52,29 @@ $(document).ready(function() {
 
 
 
-
+	
+/* script scrol up */	
+    $(document).ready(function(){
+  
+        $(window).scroll(function(){
+            if ($(this).scrollTop() > 100) {
+                $('.scrollup').fadeIn();
+            } else {
+                $('.scrollup').fadeOut();
+            }
+        
+	
+		});
+  
+        $('.scrollup').click(function(){
+            $("html, body").animate({ scrollTop: 0 }, 600);
+            return false;
+        });
+  
+ 
+    });
+	
+	/* script scrol up */
 
 
 
